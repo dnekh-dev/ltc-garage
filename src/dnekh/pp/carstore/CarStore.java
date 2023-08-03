a1e6881c54ae8754ca728f0b3104fa0c4c0245a7
d_nekh <dnekh.dev@gmail.com>
2023-08-03 18:48:04 +0900

    First implementation of enum CarStore

--- src/dnekh/pp/carstore/CarStore.java
+++ src/dnekh/pp/carstore/CarStore.java
@@ -1,4 +1,52 @@
 package dnekh.pp.carstore;
 
-public class CarStore {
+public enum CarStore {
+
+    INSTANCE;
+
+    private String name;
+    private String nameOfOwner;
+    private String email;
+    private String phoneNumber;
+    private String address;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNameOfOwner() {
+        return nameOfOwner;
+    }
+
+    public void setNameOfOwner(String nameOfOwner) {
+        this.nameOfOwner = nameOfOwner;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 }
